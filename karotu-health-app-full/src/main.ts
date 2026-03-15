f7e61f45252b9f44c7986ebbef0bb9fc854891cb
Richard Jackson <denezt@yahoo.com>
2026-03-15 17:03:01 +0100

    Updating

--- karotu-health-app-full/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideIonicAngular } from '@ionic/angular/standalone';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideIonicAngular()]
-}).catch((err) => console.error(err));
